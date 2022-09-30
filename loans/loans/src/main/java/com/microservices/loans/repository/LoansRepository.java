576f80dfbd97be3d4d044a8407755e9a9e4c914d
usmannaveed13 <usmannaveed13@gmail.com>
2022-09-30 12:04:41 +0500

    create loans service in which only one controller and repo and models have created

---
+++ loans/loans/src/main/java/com/microservices/loans/repository/LoansRepository.java
@@ -0,0 +1,12 @@
+package com.microservices.loans.repository;
+
+import com.microservices.loans.model.Loans;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LoansRepository extends CrudRepository<Loans, Long> {
+    List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
+}
