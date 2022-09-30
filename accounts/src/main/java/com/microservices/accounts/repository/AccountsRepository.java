2de494c64d2fee1738c0f39840fb911dd69d22a4
usmannaveed13 <usmannaveed13@gmail.com>
2022-09-30 12:02:57 +0500

    create accounts service in which only one controller and repo and models have created

---
+++ accounts/src/main/java/com/microservices/accounts/repository/AccountsRepository.java
@@ -0,0 +1,8 @@
+package com.microservices.accounts.repository;
+
+import com.microservices.accounts.model.Accounts;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountsRepository extends CrudRepository<Accounts, Long> {
+    Accounts findByCustomerId(int customerId);
+}
