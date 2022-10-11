423276ac5c5dbd545d33abd6737af454192b3763
usmannaveed13 <usmannaveed13@gmail.com>
2022-10-11 18:39:13 +0500

    now for getting configuration properties from config server we are making config client to accounts service and added accountsServiceConfig file and for mapping properties and also made model same as properties and bind in controller and test respond in json format

--- accounts/src/main/java/com/microservices/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/microservices/accounts/controller/AccountsController.java
@@ -1,17 +1,27 @@
 package com.microservices.accounts.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.microservices.accounts.config.AccountsServiceConfig;
 import com.microservices.accounts.model.Accounts;
 import com.microservices.accounts.model.Customer;
+import com.microservices.accounts.model.Properties;
 import com.microservices.accounts.repository.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class AccountsController {
     @Autowired
     private AccountsRepository accountsRepository;
+
+    @Autowired
+    AccountsServiceConfig accountsConfig;
 
     @PostMapping("/myAccount")
     public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -22,6 +32,14 @@ public class AccountsController {
         } else {
             return null;
         }
+    }
 
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(),
+                accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 }
