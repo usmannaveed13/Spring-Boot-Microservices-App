d4d36004bfdbd57e7f813970cb485d425fb15029
usmannaveed13 <usmannaveed13@gmail.com>
2022-10-14 18:46:33 +0500

    now we added @RefreshScope annotation for getting change from latest change of property in config github repo via config server

--- loans/src/main/java/com/microservices/loans/LoansApplication.java
+++ loans/src/main/java/com/microservices/loans/LoansApplication.java
@@ -3,11 +3,13 @@ package com.microservices.loans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.microservices.loans.controller") })
 @EnableJpaRepositories("com.microservices.loans.repository")
 @EntityScan("com.microservices.loans.model")
