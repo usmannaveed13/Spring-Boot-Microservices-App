51c7d33dba7d3840dddf590d409c84d28788eb90
usmannaveed13 <usmannaveed13@gmail.com>
2022-10-17 23:37:13 +0500

    now added openfeign dependency for getting response from other services and send your complete accounts detail

--- accounts/src/main/java/com/microservices/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/microservices/accounts/controller/AccountsController.java
@@ -4,15 +4,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.microservices.accounts.config.AccountsServiceConfig;
-import com.microservices.accounts.model.Accounts;
-import com.microservices.accounts.model.Customer;
-import com.microservices.accounts.model.Properties;
+import com.microservices.accounts.model.*;
 import com.microservices.accounts.repository.AccountsRepository;
+import com.microservices.accounts.service.client.CardsFeignClient;
+import com.microservices.accounts.service.client.LoansFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -22,6 +24,12 @@ public class AccountsController {
 
     @Autowired
     AccountsServiceConfig accountsConfig;
+
+    @Autowired
+    LoansFeignClient loansFeignClient;
+
+    @Autowired
+    CardsFeignClient cardsFeignClient;
 
     @PostMapping("/myAccount")
     public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -41,5 +49,19 @@ public class AccountsController {
                 accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
         String jsonStr = ow.writeValueAsString(properties);
         return jsonStr;
+    }
+
+    @PostMapping("/myCustomerDetails")
+    public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+        List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+        List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+
+        CustomerDetails customerDetails = new CustomerDetails();
+        customerDetails.setAccounts(accounts);
+        customerDetails.setLoans(loans);
+        customerDetails.setCards(cards);
+
+        return customerDetails;
     }
 }
