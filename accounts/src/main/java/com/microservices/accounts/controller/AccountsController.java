30016fab9cfd93aca5ceab06a2d951c5ff6ded31
usmannaveed13 <usmannaveed13@gmail.com>
2022-10-20 17:14:27 +0500

    added circuit breaker dependencies for creating resiliency in application

--- accounts/src/main/java/com/microservices/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/microservices/accounts/controller/AccountsController.java
@@ -8,6 +8,9 @@ import com.microservices.accounts.model.*;
 import com.microservices.accounts.repository.AccountsRepository;
 import com.microservices.accounts.service.client.CardsFeignClient;
 import com.microservices.accounts.service.client.LoansFeignClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,6 +55,9 @@ public class AccountsController {
     }
 
     @PostMapping("/myCustomerDetails")
+    @CircuitBreaker(name = "detailsForCustomerSupportApp",fallbackMethod
+             ="myCustomerDetailsFallBack")
+    @Retry(name = "retryForCustomerDetails", fallbackMethod = "myCustomerDetailsFallBack")
     public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
         List<Loans> loans = loansFeignClient.getLoansDetails(customer);
@@ -64,4 +70,25 @@ public class AccountsController {
 
         return customerDetails;
     }
+
+    private CustomerDetails myCustomerDetailsFallBack(Customer customer, Throwable t) {
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+        List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+        CustomerDetails customerDetails = new CustomerDetails();
+        customerDetails.setAccounts(accounts);
+        customerDetails.setLoans(loans);
+        return customerDetails;
+    }
+
+    @GetMapping("/sayHello")
+    @RateLimiter(name = "sayHello", fallbackMethod = "sayHelloFallback")
+    public String sayHello() {
+        return "Hello, Welcome to Microservices";
+    }
+
+    private String sayHelloFallback(Throwable t) {
+        return "Hi, Welcome to Microservices";
+    }
+
+
 }
