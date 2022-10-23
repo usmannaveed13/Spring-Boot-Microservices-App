961daae376d48bffa5cf5335a03126177dcc843e
usmannaveed13@gmail.com <usmannaveed13@gmail.com>
2022-10-23 19:20:24 +0500

    added custom route bean that matches and call relevant service

--- gatewayserver/src/main/java/com/microservices/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/microservices/gatewayserver/GatewayserverApplication.java
@@ -2,7 +2,13 @@ package com.microservices.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +16,26 @@ public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/microservices/accounts/**")
+						.filters(f -> f.rewritePath("/microservices/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://ACCOUNTS")).
+						route(p -> p
+								.path("/microservices/loans/**")
+								.filters(f -> f.rewritePath("/microservices/loans/(?<segment>.*)","/${segment}")
+										.addResponseHeader("X-Response-Time",new Date().toString()))
+								.uri("lb://LOANS")).
+						route(p -> p
+								.path("/microservices/cards/**")
+								.filters(f -> f.rewritePath("/microservices/cards/(?<segment>.*)","/${segment}")
+										.addResponseHeader("X-Response-Time",new Date().toString()))
+								.uri("lb://CARDS")).build();
 	}
 
 }
