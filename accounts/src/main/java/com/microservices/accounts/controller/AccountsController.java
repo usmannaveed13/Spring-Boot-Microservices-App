b42e0852bb5a78d419f4a1a94ed2ada73ff35f2a
usmannaveed13 <usmannaveed13@gmail.com>
2022-10-31 17:09:43 +0500

    added micrometer,prometheus & aop dependency for getting health metrices of microservices

--- accounts/src/main/java/com/microservices/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/microservices/accounts/controller/AccountsController.java
@@ -11,6 +11,7 @@ import com.microservices.accounts.service.client.LoansFeignClient;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.micrometer.core.annotation.Timed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,7 @@ public class AccountsController {
     CardsFeignClient cardsFeignClient;
 
     @PostMapping("/myAccount")
+    @Timed(value = "getAccountDetails.time", description = "Time taken to return Account Details")
     public Accounts getAccountDetails(@RequestBody Customer customer) {
 
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
